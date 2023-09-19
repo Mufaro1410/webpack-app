a6fe15b706e3efc44ba7405afc658f7c8cfd6c8b
Mufaro1410 <fungurayimufaro@gmail.com>
2023-09-19 16:28:52 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import generateJoke from "./generateJoke";
+import './styles/main.scss';
+import laughing from './assets/laughing.svg'
+
+document.getElementById('laughImg').src = laughing
+document.getElementById('jokeBtn').addEventListener('click', generateJoke)
+
+generateJoke()
